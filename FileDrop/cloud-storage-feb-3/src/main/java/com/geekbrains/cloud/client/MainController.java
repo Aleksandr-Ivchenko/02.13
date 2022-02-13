d944c788c74c54eb6957bfd3a689fc1388c6a370
Aleksandr-Ivchenko <85690047+Aleksandr-Ivchenko@users.noreply.github.com>
2022-02-13 13:34:15 +0300

    Add files via upload

---
+++ FileDrop/cloud-storage-feb-3/src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -0,0 +1,142 @@
+package com.geekbrains.cloud.client;
+
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    private static final int BUFFER_SIZE = 8192;
+
+    public TextField clientPath;
+    public TextField serverPath;
+    public javafx.scene.control.ListView<String> clientView;
+    public ListView<String> serverView;
+    private File currentDirectory;
+    private File serverDirectory;
+
+    private DataInputStream is;
+    private DataOutputStream os;
+    private byte[] buf;
+
+//     Platform.runLater(() -> {})
+    private void updateClientView() {
+
+            clientPath.setText(currentDirectory.getAbsolutePath());
+            clientView.getItems().clear();
+            clientView.getItems().add("...");
+            clientView.getItems()
+                    .addAll(currentDirectory.list());
+        };
+
+
+    private void updateServerView() {
+
+            serverPath.setText(serverDirectory.getPath());
+            serverView.getItems().clear();
+            serverView.getItems().add("...");
+            serverView.getItems()
+                    .addAll(serverDirectory.list());
+        };
+
+    public void download(ActionEvent actionEvent) throws IOException {
+        String item = serverView.getSelectionModel().getSelectedItem();
+        File selected = serverDirectory.toPath().resolve(item).toFile();
+        if (selected.isFile()) {
+            os.writeUTF("#file_download#");
+            os.writeUTF(selected.getName());
+            os.writeLong(selected.length());
+            try (InputStream fis = new FileInputStream(selected)) {
+                while (fis.available() > 0) {
+                    int readBytes = fis.read(buf);
+                    os.write(buf, 0, readBytes);
+                }
+            }
+            os.flush();
+            updateClientView();
+        }
+
+    }
+
+    // upload file to server
+    public void upload(ActionEvent actionEvent) throws IOException {
+        String item = clientView.getSelectionModel().getSelectedItem();
+        File selected = currentDirectory.toPath().resolve(item).toFile();
+        if (selected.isFile()) {
+            os.writeUTF("#file_message#");
+            os.writeUTF(selected.getName());
+            os.writeLong(selected.length());
+            try (InputStream fis = new FileInputStream(selected)) {
+                while (fis.available() > 0) {
+                    int readBytes = fis.read(buf);
+                    os.write(buf, 0, readBytes);
+                }
+            }
+            os.flush();
+            updateServerView();
+        }
+    }
+
+    private void initNetwork() {
+        try {
+            buf = new byte[BUFFER_SIZE];
+            Socket socket = new Socket("localhost", 8189);
+            is = new DataInputStream(socket.getInputStream());
+            os = new DataOutputStream(socket.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        currentDirectory = new File(System.getProperty("user.home"));
+        serverDirectory = new File("server");
+
+
+        // run in FX Thread
+        // :: - method reference
+        updateClientView();
+        updateServerView();
+        initNetwork();
+
+        clientView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2) {
+                String item = clientView.getSelectionModel().getSelectedItem();
+                if (item.equals("...")) {
+                    currentDirectory = currentDirectory.getParentFile();
+                    updateClientView();
+                } else {
+                    File selected = currentDirectory.toPath().resolve(item).toFile();
+                    if (selected.isDirectory()) {
+                        currentDirectory = selected;
+                        updateClientView();
+                    }
+                }
+            }
+        });
+
+        serverView.setOnMouseClicked(e -> {
+            if (e.getClickCount() == 2) {
+                String item = serverView.getSelectionModel().getSelectedItem();
+                if (item.equals("...")) {
+                    serverDirectory = serverDirectory.getParentFile();
+                    updateServerView();
+                } else {
+                    File selected = serverDirectory.toPath().resolve(item).toFile();
+                    if (selected.isDirectory()) {
+                        serverDirectory = selected;
+                        updateServerView();
+                    }
+                }
+            }
+        });
+    }
+}
